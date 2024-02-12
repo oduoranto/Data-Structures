2a05a90479d6fb572bb5f39d3b25eb84c1e5feff
antony oduor <antonyoduor72@gmail.com>
2024-02-12 08:10:20 +0300

    Binary Search

--- Java.util-Arrays/src/oduor/Main.java
+++ Java.util-Arrays/src/oduor/Main.java
@@ -19,6 +19,14 @@ public class Main {
         System.out.println(Arrays.toString(firstArray));
 
 
+        String [] names = {"Anto", "Davis","Johnny", "Libra"};
+
+        if(Arrays.binarySearch(names, "Anto") >= 0){
+            System.out.println("Found on the list");
+        }else{
+            System.out.println("Not found on the ist");
+        }
+
 
 
     }
