85a0345ddb312e0cba656deb805c9efa8fe2149a
antony oduor <116315398+oduoranto@users.noreply.github.com>
2024-05-23 09:42:04 +0300

    List

---
+++ Exercise45.py
@@ -0,0 +1,12 @@
+import random
+print("Welcome to the Banker Roulette")
+
+names = ["Anto", "Johnny", "Kevo", "Eugo", "Emma"]
+number = random.randint(0,4)
+print( "The person paying is : " + names[number])
+
+cars = ["Mazda", "BMW", "Porshe", "Bugattii"]
+bike = ["Suzuki", "Dugatti","Boxer" ]
+
+vehicles = [cars, bike]
+print(vehicles)
